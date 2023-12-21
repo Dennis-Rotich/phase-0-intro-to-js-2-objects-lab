c40d6b020d94cbd88d15d646cfc4cbd6774e7beb
Dennis <denkipyator@gmail.com>
2023-12-21 09:16:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+let employee = {
+    name:'Dennis',
+    streetAddress:'78-Kapsoya'
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return{...employee,[key]:value};
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee,key){
+    let newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+
 // Write your solution in this file!
